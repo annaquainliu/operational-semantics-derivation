30bc722fffe6bf4458d0592f5fcf4e57eb4a6f40
annaquainliu <annaquainliu@gmail.com>
2023-06-30 11:59:05 -0700

    THE TICKS WORK

--- derivation.js
+++ derivation.js
@@ -25,7 +25,7 @@ button.addEventListener('click', () => {
         return;
     }
     addValuesToQueue(value);
-    const derivation = derive(Queue.pop(), true);
+    const derivation = derive(Queue.pop(), true, {"rho_ticks" : 0, "xi_ticks" : 0});
     console.log("derivation is", derivation);
     latexOutput.innerText = derivation.derivation;
 });
@@ -49,24 +49,27 @@ function addVariables(input) {
     return env;
 }
 
-function derive(exp, execute) {
+// ticks carry the ticks from before
+function derive(exp, execute, ticks) {
 
     if (/^\d+$/.test(exp)) {
-        return LIT(parseInt(exp));
+        return LIT(parseInt(exp), ticks);
     }
    
     switch (exp) {
         case "if":
-            return IF(execute);
+            return IF(execute, ticks);
         case "set":
-            return SET(execute);
+            return SET(execute, ticks);
         //     break;
         // case "begin":
         //     return BEGIN();
         default:
-           return VAR(exp);
+           return VAR(exp, ticks);
     }
 }
+
+// UTILITIES
 
 function searchEnv(variable, env) {
     for (const key in env) {
@@ -78,7 +81,6 @@ function searchEnv(variable, env) {
 }
 
 function findVarInfo(variable) {
-
     let environments = {"rho" : rho, "xi" : xi}
     for (const name in environments) {
         let value = searchEnv(variable, environments[name]);
@@ -87,42 +89,59 @@ function findVarInfo(variable) {
         } 
     }
     throw new Error(`${variable} cannot be found in either xi or rho.`);
-    
 }
 
-function LIT(number) {
+function addTicks(derivation, ticks, match) {
+    //rho, xi
+    for (const key in ticks) {
+        let env = key.split('_')[0];
+        if (ticks[key] == 0) {
+            derivation = derivation.replaceAll(env + match, env + "");
+        } else {
+            derivation = derivation.replaceAll(env + match, env + "'".repeat(ticks[key]));
+        }
+    }
+    return derivation;
+}
+// END OF UTILITIES
+
+function LIT(number, ticks) {
     let derivation = inferenceRules.literal;
     derivation = derivation.replaceAll("$v", number);
+    derivation = addTicks(derivation, ticks, "_1");
     return {"syntax" : `Literal(${number})`, 
             "value" : number,
             "derivation" : derivation};
 }
 
-function VAR(name) {
+function VAR(name, ticks) {
     let variable = findVarInfo(name);
     let derivation = inferenceRules.var;
     derivation = derivation.replaceAll("$x", name);
     if (variable.env == "rho") {
         derivation = derivation.replace("{Var}", "{FormalVar}");
-        derivation = derivation.replaceAll("$env", "\\rho");
+        derivation = derivation.replaceAll("$env", "\\rho_1");
     } else if (variable.env == "xi") {
         derivation = derivation.replace("{Var}", "{GlobalVar}");
-        derivation = derivation.replaceAll("$env", "\\xi");
+        derivation = derivation.replaceAll("$env", "\\xi_1");
     }
-
+    derivation = addTicks(derivation, ticks, "_1");
     return {"syntax" : `Var(${name})`, 
             "value" : variable.value, 
             "derivation" : derivation,
             "name" : name};
 }
 
-function IF(execute) {
-    const condition = derive(Queue.pop(), execute);
-    const trueCase = derive(Queue.pop(), condition.value != 0 && execute);
-    const falseCase = derive(Queue.pop(), condition.value == 0 && execute);
+function IF(execute, ticks) {
     let derivation = inferenceRules.if;
     let syntax = "If(e_1, e_2, e_3)";
     let value;
+    derivation = addTicks(derivation, ticks, "_1"); 
+    const condition = derive(Queue.pop(), execute, ticks);
+    const trueCase = derive(Queue.pop(), condition.value != 0 && execute, ticks);
+    const falseCase = derive(Queue.pop(), condition.value == 0 && execute, ticks);
+     //ticks obj is changed by reference
+    derivation = addTicks(derivation, ticks, "_2");
 
     function editDerivation(title, equal, branch) {
         derivation = derivation.replace("{If}", title);
@@ -131,6 +150,7 @@ function IF(execute) {
         derivation = derivation.replace("$v_r", branch.value);
         value = branch.value;
     }
+
     if (condition.value == 0) {
         editDerivation("{IfFalse}", "=", falseCase);
     } else {
@@ -142,25 +162,33 @@ function IF(execute) {
     syntax = syntax.replace("e_2", trueCase.syntax);
     syntax = syntax.replace("e_3", falseCase.syntax);
     derivation = derivation.replace("$syntax", syntax);
-    return {"syntax" : syntax, "value" : value, "derivation" : derivation};
+    return {"syntax" : syntax, 
+            "value" : value, 
+            "derivation" : derivation};
 }
 
-function SET(execute) {
-    const variable = derive(Queue.pop());
-    const exp = derive(Queue.pop());
+function SET(execute, ticks) {
     let derivation  = inferenceRules.set;
-    console.log(variable);
+    derivation = addTicks(derivation, ticks, "_1");
+    //ticks are changed from the derive process
+    const variable = derive(Queue.pop(), execute, ticks); 
+    const exp = derive(Queue.pop(), execute, ticks);
     const env = findVarInfo(variable.name).env;
     if (execute) {  
         let environment = {"rho" : rho, "xi" : xi};
         environment[env][variable.name] = exp.value;
+        //add the tick for the change in environment and assign formal title
+        if (env == "rho") {
+            derivation = derivation.replace("{Assign}", "{FormalAssign}");
+        } 
+        else {
+            derivation = derivation.replace("{Assign}", "{GlobalAssign}");
+        }
+        derivation = derivation.replace(`${env}_2`, `${env}_2\\{${variable.name}\\mapsto${exp.value}\\}`);
     }
-    let rho_tick = 0; let xi_tick = 0;
-    if (env == "rho") {
-        derivation = derivation.replace("{Assign}", "{FormalAssign}");
-    } 
-    else {
-        derivation = derivation.replace("{Assign}", "{GlobalAssign}");
+    derivation = addTicks(derivation, ticks, "_2");
+    if (execute) {
+        ticks[`${env}_ticks`]++;
     }
     derivation = derivation.replace("$Scope", `${variable.name} \\in dom \\${env}`);
     derivation = derivation.replace("$exp_derivation", exp.derivation);
@@ -170,5 +198,5 @@ function SET(execute) {
 
     return {"syntax" : `Set(${variable.syntax}, ${exp.syntax})`,
             "value" : exp.value,
-            "derivation" : derivation}
+            "derivation" : derivation};
 }
