b845c82c315a845a10e9df95a3fa0c4bbde55c8a
annaquainliu <annaquainliu@gmail.com>
2023-07-12 20:10:32 -0700

    buttons! :3

--- derivation.js
+++ derivation.js
@@ -3,7 +3,6 @@ const latexButton  = document.getElementById('deriveLatex');
 const HtmlButton = document.getElementById('deriveHTML');
 const latexOutput = document.getElementById("latex");
 const HtmlOutput = document.getElementById('HTMLOutput');
-const variables = document.getElementsByClassName('variable');
 import HtmlElement from './htmlRenderer/htmlElement.js';
 import Latex from './latexRenderer/latex.js';
 import Rules from './htmlRenderer/inferenceRules.js';
@@ -30,66 +29,17 @@ window.onload = () => {
     });
 }
 
-function isValidName(name) {
-    if (name.startsWith("$")) {
-        alert(`You cannot start your variable name with a '$'.`);
-        return false;
-    }
-    let invalidWords = ['begin', 'if', 'set', 'while', 'val', '+', '-', "/", "=", "*", "||", "&&", "mod"];
-    if (invalidWords.includes(name)) {
-        alert(`You cannot name your variable '${name}', as it is an Impcore keyword.`);
-        return false;
-    }
-    return true;
-}
-
-Array.prototype.forEach.call(variables, variableDiv => {
-    const variableInfo = variableDiv.getElementsByClassName('variable_info')[0];
-    const addVariable = variableDiv.getElementsByClassName('add_variable')[0];
-    const button = variableDiv.getElementsByTagName('button')[0];
-    const env = variableDiv.getAttribute('value');
-    const nameField = variableDiv.querySelector("input[name='name']");
-    const valueField = variableDiv.querySelector("input[name='value']");
-
-    addVariable.addEventListener('click', () => {
-        if (variableInfo.style.display == "none" || variableInfo.style.display == "") {
-            variableInfo.style.display = "flex";
-        } else {
-            variableInfo.style.display = "none";
-        }
-    });
-    button.addEventListener('click', () => {
-        const name = nameField.value.replaceAll(" ", "");
-        const value = valueField.value;
-        if (name == "" || value == "") {
-            alert("Please fill in the name and the value of the variable");
-            return;
-        }
-        if (!isValidName(name)) {
-            return;
-        }
-        let currentValue = document.getElementById(env).value;
-        let addOn = "";
-        if (currentValue == " {}") {
-            addOn = `${name} → ${value}}`
-        } else {
-            addOn = `, ${name} → ${value}}`
-        }
-        document.getElementById(env).value = currentValue.substring(0, currentValue.length - 1) + addOn;
-    });
-});
-
 function addVariablesToEnv() {
     xi = {}; rho = {}; // clear environments
     ticks = {rho_ticks : 0, xi_ticks : 0};
     let environments = {"xi" : xi, "rho" : rho};
     ["rho", "xi"].forEach(env => {
-        let variables = document.getElementById(env).value;
-        variables = variables.replaceAll(" ", "");
-        variables = variables.substring(1, variables.length - 1);
-        const maps = variables.split(',');
-        maps.forEach(mapping => {
-            const fields = mapping.split('→');
+        const variables = document.getElementById(env).getElementsByClassName('varAndvalue');
+        Array.prototype.forEach.call(variables, mapping => {
+            const fields = mapping.innerText.replaceAll(" ", "").split('→');
+            if (fields.length != 2) {
+                return;
+            }
             const name = fields[0];
             const value = fields[1];
             environments[env][name] = parseInt(value);
---
+++ frontend/frontend.js
@@ -0,0 +1,63 @@
+const variables = document.getElementsByClassName('variable');
+
+Array.prototype.forEach.call(variables, variableDiv => {
+    const variableInfo = variableDiv.getElementsByClassName('variable_info')[0];
+    const addVariable = variableDiv.getElementsByClassName('add_variable')[0];
+    const button = variableDiv.getElementsByTagName('button')[0];
+    const env = variableDiv.getAttribute('value');
+    const nameField = variableDiv.querySelector("input[name='name']");
+    const valueField = variableDiv.querySelector("input[name='value']");
+
+    addVariable.addEventListener('click', () => {
+        if (variableInfo.style.display == "none" || variableInfo.style.display == "") {
+            variableInfo.style.display = "flex";
+        } else {
+            variableInfo.style.display = "none";
+        }
+    });
+    //add variable
+    button.addEventListener('click', () => {
+        const name = nameField.value.replaceAll(" ", "");
+        const value = valueField.value;
+        if (name == "" || value == "") {
+            alert("Please fill in the name and the value of the variable");
+            return;
+        }
+        if (!isValidName(name)) {
+            return;
+        }
+        const mapping = document.getElementById(env);
+        const map = makeMapDiv(name, parseInt(value), mapping, env);
+        mapping.appendChild(map);
+    });
+});
+
+function makeMapDiv(name, value, parent, env) {
+    const div = document.createElement('div');
+    div.className = "map map-" + env;
+    const varAndValue = document.createElement('span');
+    varAndValue.className = "varAndvalue";
+    varAndValue.innerText = `${name} → ${value}`;
+    const deleteDiv = document.createElement('span');
+    deleteDiv.className = 'delete';
+    deleteDiv.innerText = "x";
+    div.append(varAndValue, deleteDiv);
+    deleteDiv.addEventListener('click', () => {
+       parent.removeChild(div);
+    });
+    return div;
+}
+
+
+function isValidName(name) {
+    if (name.startsWith("$")) {
+        alert(`You cannot start your variable name with a '$'.`);
+        return false;
+    }
+    let invalidWords = ['begin', 'if', 'set', 'while', 'val', '+', '-', "/", "=", "*", "||", "&&", "mod"];
+    if (invalidWords.includes(name)) {
+        alert(`You cannot name your variable '${name}', as it is an Impcore keyword.`);
+        return false;
+    }
+    return true;
+}
